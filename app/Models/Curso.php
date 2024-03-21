48e092e91218aa39586375a915264773ed887c97
carlos1510 <carloscle1510@gmail.com>
2024-03-21 15:19:42 -0500

    Agrega modelos y Seeder y Factorys

---
+++ app/Models/Curso.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Curso extends Model
+{
+    use HasFactory;
+
+    //protected $table = "users"; si en caso no se quiere utilizar la convención
+}
